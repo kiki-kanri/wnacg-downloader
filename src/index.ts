19878c2106d1f8bb8ac5e0d187ba493573d9ab4f
kiki-kanri <a470666@gmail.com>
2023-07-20 04:11:06 +0800

    Set max listeners to 0.

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import readlineSync from 'readline-sync';
 
 import Downloader from '@/classes/downloader';
 
+process.setMaxListeners(0);
+
 async function main() {
 	while (true) {
 		const aidsOrUrlsString = readlineSync.question('請輸入網址或aid(多個請用空格分隔)：').trim();
