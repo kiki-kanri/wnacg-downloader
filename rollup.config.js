937fe10716dc4eaba03b497e1494e96fa191e1fc
kiki-kanri <a470666@gmail.com>
2023-07-20 05:15:24 +0800

    Edit rollup config.

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,14 @@
-import { defineConfig } from 'rollup';
 import commonjs from '@rollup/plugin-commonjs';
-import externals from 'rollup-plugin-node-externals';
-import path from 'path';
 import resolve from '@rollup/plugin-node-resolve';
 import strip from '@rollup/plugin-strip';
 import terser from '@rollup/plugin-terser';
 import typescript from '@rollup/plugin-typescript';
+import path from 'path';
+import { defineConfig } from 'rollup';
+import externals from 'rollup-plugin-node-externals';
 
-const srcPath = path.resolve(__dirname, 'src', 'index.ts');
 const distPath = path.resolve(__dirname, 'dist');
-const production = !process.env.ROLLUP_WATCH;
+const srcPath = path.resolve(__dirname, 'src', 'index.ts');
 
 export default defineConfig({
 	input: srcPath,
@@ -25,7 +24,7 @@ export default defineConfig({
 		strip({
 			include: ['**/*.ts']
 		}),
-		production && terser({
+		terser({
 			module: true
 		}),
 		typescript({
