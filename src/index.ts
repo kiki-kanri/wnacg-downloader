a1eafc1438f7056c7fd63e593f39ea2094162161
kiki-kanri <a470666@gmail.com>
2023-08-14 01:17:57 +0800

    Change question aid or url method.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,23 @@
+import inquirer, { QuestionCollection } from 'inquirer';
 import logger from 'node-color-log';
-import readlineSync from 'readline-sync';
 
 import Downloader from '@/classes/downloader';
 
 process.setMaxListeners(0);
 
+interface PromptAnswers {
+	aidsOrUrlsString: string;
+}
+
+const promptOptions: QuestionCollection<PromptAnswers> = {
+	message: '請輸入網址或aid(多個請用空格分隔)：',
+	name: 'aidsOrUrlsString'
+};
+
 async function main() {
 	while (true) {
-		const aidsOrUrlsString = readlineSync.question('請輸入網址或aid(多個請用空格分隔)：').trim();
+		const answers = await inquirer.prompt(promptOptions);
+		const aidsOrUrlsString = answers.aidsOrUrlsString.trim();
 		const aidsOrUrls = aidsOrUrlsString.split(' ');
 		for (const aidOrUrl of aidsOrUrls) {
 			if (!aidOrUrl.trim()) continue;
