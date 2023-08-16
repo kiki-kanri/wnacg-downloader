b4399f621eda86f1ff9b42c05644e214bb9f1516
kiki-kanri <a470666@gmail.com>
2023-08-16 12:36:44 +0800

    Edit configs and remove packages.

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,3 @@
-import commonjs from '@rollup/plugin-commonjs';
-import resolve from '@rollup/plugin-node-resolve';
 import strip from '@rollup/plugin-strip';
 import path from 'path';
 import { defineConfig } from 'rollup';
@@ -10,28 +8,24 @@ import { fileURLToPath } from 'url';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
-const distPath = path.resolve(__dirname, 'dist');
-const srcPath = path.resolve(__dirname, 'src', 'index.ts');
+const distPath = path.join(__dirname, 'dist');
+const inputPath = path.join(__dirname, 'src', 'index.ts');
 
 export default defineConfig({
 	// Use this setting to set the package as an external package, such as fs-extra, lodash...etc.
 	// Docs: https://rollupjs.org/configuration-options/#external
 	external: [],
-	input: srcPath,
+	input: inputPath,
 	output: {
 		dir: distPath,
 		name: 'index.js',
 		format: 'es'
 	},
 	plugins: [
-		commonjs(),
-		esbuild({
-			minify: true,
-			target: 'esnext'
-		}),
+		// Must remove debugger statements before other plugins.
+		strip({ include: ['./src/**/*.ts'] }),
+		esbuild({ minify: true }),
 		externals(),
-		resolve({ extensions: ['.js', '.json', '.mjs', '.node', '.ts'] }),
-		strip({ include: ['./src/**/*'] }),
 		typescriptPaths({ preserveExtensions: true })
 	]
 });
