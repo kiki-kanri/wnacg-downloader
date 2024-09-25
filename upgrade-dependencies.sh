497f0f62f8a3fb6009288d05eb299c91f89ef442
kiki-kanri <a470666@gmail.com>
2024-09-25 23:11:16 +0800

    chore: add and update scripts

--- upgrade-dependencies.sh
+++ upgrade-dependencies.sh
@@ -2,7 +2,7 @@
 
 . ./.env.development.local
 [ -z "$NPM_REGISTRY" ] && NPM_REGISTRY='https://registry.npmjs.org'
-export NPM_REGISTRY="$NPM_REGISTRY"
+export NPM_REGISTRY
 pnpm upgrade -L --lockfile-only
 bun i
 bun update
