555c2221b86863a772f05cf83d58048793f336fe
kiki-kanri <a470666@gmail.com>
2024-10-09 13:02:13 +0800

    chore: update build-optimization.sh to support sh

--- build-optimization.sh
+++ build-optimization.sh
@@ -1,14 +1,21 @@
-#!/bin/bash
+#!/bin/sh
 
 export NODE_ENV=production
 
-[[ "$*" == *'--clean'* ]] && rm -rf ./dist
+case "$*" in
+*--clean*)
+	rm -rf ./dist
+	;;
+esac
 
 bun run build:exec --outdir ./dist ./src/index.ts &&
 	bunx ts-project-builder ./dist/index.js -f esm --minify || exit 1
 
-if [[ "$*" == *'--compile'* ]]; then
+case "$*" in
+*--compile*)
 	bun run build:exec --bytecode --compile --outfile ./dist/index ./dist/index.mjs
-else
+	;;
+*)
 	bun run build:exec --bytecode --outdir ./dist ./dist/index.mjs
-fi
+	;;
+esac
